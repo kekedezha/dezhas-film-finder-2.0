200acb4d28b25d7fc0d785f80e90d2b75c25a1ae
kekedezha <dezhac6@gmail.com>
2024-05-02 17:10:43 -0400

    finised genreSelection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { GenreForm } from "@/components/GenreSelection";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedGenre, setSelectedGenre] = useState<string>("");
+
   return (
     <main className="flex min-h-screen flex-col items-center pt-10 px-24">
       <div className="z-10 w-full max-w-5xl mb-3 items-center justify-between font-sans text-sm lg:flex">
@@ -18,9 +24,8 @@ export default function Home() {
           Select genres and/or add more filters to generate your next movie to watch!
       </p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <p>
-        </p>
+      <div className="min-h-80 text-center flex place-items-center">
+        <GenreForm />
       </div>
 
       <div className="fixed bottom-6 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
---
+++ src/components/GenreSelection.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+// API key for The Movie Database
+const tmdbKey = process.env.NEXT_PUBLIC_TMDB_KEY;
+// The Movie Database beginning url 
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+// Genre Form Schema
+const GenreFormSchema = z.object({
+    genre: z
+        .string({
+            required_error: "Please select a genre!"
+        })
+})
+
+export function GenreForm() {
+    // Initialize state with an empty array of the specified type
+    const [genres, setGenres] = useState<string[]>([]);
+
+    const form = useForm<z.infer<typeof GenreFormSchema>>({
+        resolver: zodResolver(GenreFormSchema),
+    })
+
+    useEffect(() => {
+
+        const getGenres = async() => {
+            const genreRequestEndpoint = '/genre/movie/list';
+            // Query string that will be appended to end of url 
+            const requestParam = `?api_key=${tmdbKey}`;
+            // Full url to get movie genres
+            const urlToFetch = tmdbBaseUrl+genreRequestEndpoint+requestParam;
+        
+            try 
+            {
+                // Await GET request and save promise to response
+                const response = await fetch(urlToFetch);
+                // If promise has a truthy value then execute if statement
+                if(response.ok) {
+                    // Await the response promise to be converted to a json object.
+                    const jsonResponse = await response.json();
+                    const fetchedGenres = jsonResponse.genres.map((genre: { name: string }) => genre.name);
+                    setGenres(fetchedGenres); // Update genres state with fetched genres
+                }
+            } catch(error) {
+                console.log(error);
+            }
+        }
+
+        getGenres();
+
+    }, [])
+
+
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+                <FormField 
+                    control={form.control}
+                    name="genre"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Select Movie Genre: </FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="What genre are you feeling?" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {genres.map((genreItem) => (
+                                        <SelectItem key={genreItem} value={genreItem}>{genreItem}</SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <FormDescription />
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Generate</Button>
+            </form>
+        </Form>
+    )
+}
